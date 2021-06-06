e9baba2cd2bc2b7fa54ba78e57b2607d9d7f2632
Cuong Duong <cuong@canva.com>
2021-06-06 17:06:53 +1000

    update pipeline script

--- commonlit/pipeline.py
+++ commonlit/pipeline.py
@@ -1,15 +1,6 @@
-from .predict import load_test_data, predict
-from .train import load_train_data, preprocess, train
-
-
-def main():
-    train_df = load_train_data()
-    model = train(
-        preprocess(train_df), "target", exclude_features=["id", "excerpt", "standard_error"]
-    )
-    test_df = load_test_data()
-    predict(model, test_df, exclude_features=["id", "excerpt"])
-
+from . import train
+from . import predict
 
 if __name__ == "__main__":
-    main()
+    train.main()
+    predict.main()
